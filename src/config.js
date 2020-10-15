9b5cc8a989de777230eefbbfd85d73f4315874bb
Kukieu <stiekerosiem@gmail.com>
2020-10-15 17:42:29 +0200

    add heroku server

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BACKEND = {
-    ADDRESS: 'http://localhost:3000'
+    // ADDRESS: 'http://localhost:3000'
+    ADDRESS: 'https://gamifyswim-backend.herokuapp.com'
     // ADDRESS: 'http://192.168.1.28:3000'
 };
 
