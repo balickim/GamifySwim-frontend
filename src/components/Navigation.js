ccc0fb3d50d22048fc99d0c487e59d3beb36d1b6
Kukieu <stiekerosiem@gmail.com>
2020-11-27 00:44:47 +0100

    refactor to functional component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,27 +3,13 @@ import { connect } from 'react-redux';
 import Topbar from './Topbar';
 import Sidebar from './Sidebar/Sidebar';
 
-// const Navigation = (props) => {
-//     console.log('%cNavigation.js line:8 props.userRole', 'color: #007acc;', props.userRole);
-//     return (
-        // <div>
-        //     <Topbar userRole={props.userRole}/>
-        //     <Sidebar userRole={props.userRole} />
-        // </div>
-//     )
-// }
-
-// export default Navigation
-
-class Navigation extends React.Component {
-    render() {
-        return (
-            <div>
-                <Topbar userRole={this.props.account.roleId}/>
-                <Sidebar userRole={this.props.account.roleId} />
-            </div>
-        )
-    }
+function Navigation(props) {
+    return (
+        <div>
+            <Topbar userRole={props.account.roleId}/>
+            <Sidebar userRole={props.account.roleId} />
+        </div>
+    )
 };
 
 
